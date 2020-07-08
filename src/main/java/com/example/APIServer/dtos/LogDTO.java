1a89f3cf7489592d8082a1fcd4c090315caf5904
AnastasiaSperanskaya <cookie003@mail.ru>
2020-07-08 12:55:22 +0500

    dtos changed due to lombok installation

--- src/main/java/com/example/APIServer/dtos/LogDTO.java
+++ src/main/java/com/example/APIServer/dtos/LogDTO.java
@@ -1,4 +1,15 @@
 package com.example.APIServer.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LogDTO {
+    private int id;
+    private int userID;
+    private long changedTime;
+    private String newStatus;
 }
--- src/main/java/com/example/APIServer/dtos/UserDTO.java
+++ src/main/java/com/example/APIServer/dtos/UserDTO.java
@@ -1,40 +1,14 @@
 package com.example.APIServer.dtos;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
-
-    private Long id;
-
-    @NonNull
-    private String username;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String userName;
     private String email;
-
-    public UserDTO(String username, String email) {
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setId(Object id) {
-        this.id = (Long) id;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
+    private String status;
 }
