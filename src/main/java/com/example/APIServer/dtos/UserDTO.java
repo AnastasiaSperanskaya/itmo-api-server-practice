d66fa9e684aafa6fe7b7d42da677cdf36aa6de62
AnastasiaSperanskaya <cookie003@mail.ru>
2020-07-08 09:33:49 +0500

    UserDTO added

--- src/main/java/com/example/APIServer/dtos/UserDTO.java
+++ src/main/java/com/example/APIServer/dtos/UserDTO.java
@@ -1,4 +1,21 @@
 package com.example.APIServer.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
+
+    private Long id;
+
+    @NonNull
+    private String username;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String email;
 }
