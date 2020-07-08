6cd00c45e2729c5720cce6dffd35dfd50758962d
AnastasiaSperanskaya <cookie003@mail.ru>
2020-07-08 10:01:35 +0500

    Status Initializer added

---
+++ src/main/java/com/example/APIServer/initializers/StatusInitializer.java
@@ -0,0 +1,29 @@
+package com.example.APIServer.initializers;
+
+import com.example.APIServer.entities.StatusEntity;
+import com.example.APIServer.Status;
+import com.example.APIServer.repositories.StatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StatusDataInitializerComponent implements ApplicationRunner {
+
+    private final StatusRepository statusRepository;
+
+    @Autowired
+    public StatusDataInitializerComponent(StatusRepository statusRepository) {
+        this.statusRepository = statusRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (statusRepository.count() == 0) {
+            for (Status status: Status.values()) {
+                statusRepository.save(new StatusEntity(status));
+            }
+        }
+    }
+}
--- src/main/java/com/example/APIServer/repositories/UserRepository.java
+++ src/main/java/com/example/APIServer/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.APIServer.repositories;
 import org.springframework.data.repository.CrudRepository;
 import com.example.APIServer.entities.UserEntity;
 import com.example.APIServer.entities.StatusEntity;
-import com.example.APIServer.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
